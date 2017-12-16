39279a7d62629d6555cf4ab2589d39112268af7b
Geromy Cunningham <geromycunningham@gmail.com>
2017-12-16 16:37:25 +0000

    push

---
+++ FCC_booWho.js
@@ -0,0 +1,13 @@
+
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  if(bool === true){
+    return true;
+  } else if(bool === false){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+booWho(null);
